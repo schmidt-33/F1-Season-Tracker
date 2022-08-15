cc60dc6b2d4499969d77e48d6b28af5e079fc9ca
schmidt-33 <33700475+schmidt-33@users.noreply.github.com>
2022-08-15 12:28:43 -0400

    small css class name changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,14 @@ import { NavLink } from "react-router-dom"
 
 function Navbar() {
   return (
-    <nav className='navbar'>
+    <nav className='background'>
+      <div className='navbar'>
         <h1>F1 Paddock Tracker</h1>
         <div className='nav-links'>
             <NavLink to='/races'>Races</NavLink>
-            <NavLink to='/standings/drivers'>Standings</NavLink>
+            <NavLink to='/standings/'>Standings</NavLink>
         </div>
+      </div>
     </nav>
   )
 }
