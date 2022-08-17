72cc587d5b0d9e5dfba07e16f1cf9d3dd32adc23
schmidt-33 <33700475+schmidt-33@users.noreply.github.com>
2022-08-17 13:23:36 -0400

    added styling for drivers standings

--- src/components/Drivers.tsx
+++ src/components/Drivers.tsx
@@ -1,27 +1,52 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+
 function Drivers() {
 	const [Drivers, setDrivers] = useState([])
+	const team = 'teamcolor'
 
+	//get the information of the driver standings and set it to Drivers array
 	useEffect(() => {
-		axios.get('http://ergast.com/api/f1/current/drivers.json')
+		axios.get('http://ergast.com/api/f1/current/driverstandings.json')
 			.then(res => {
-				console.log(res.data.MRData.DriverTable.Drivers)
-				setDrivers(res.data.MRData.DriverTable.Drivers)
+				console.log(res.data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
+				setDrivers((res.data.MRData.StandingsTable.StandingsLists[0].DriverStandings))
+				
 			})
 			.catch(err => {
 				console.log(err)
 			})
 	}, [])
 
+	console.log(Drivers)
 	return (
-		<div>
-			{
-				Drivers.map(driver => <div key={driver.driverId}> {driver.givenName} {driver.familyName} </div>)
-			}
-		</div>
+			<div className="standing-container">
+				{
+					Drivers.map(driver =>
+						<div key={driver.position} className={`${driver.Constructors[0].constructorId} standing-line`}>
+							<div>
+								{driver.Driver.givenName} {driver.Driver.familyName} 
+							</div>
+							<div>
+								{driver.points}
+							</div>
+						</div>
+					)
+				}
+			</div>
 	)
 }
+
+// //convert team names with _ to - for class name usage
+// function nameConversion(teamName) {
+// 	let team = teamName
+
+// 	for(let i = 0; i < team; i++){
+// 		if(team[i] == "_")
+// 			team[i] = "-"
+// 		return team
+// 	}
+// }
 
 export default Drivers
