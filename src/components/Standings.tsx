95b97a8294eaca6c27e6478a86927f7e5337cdaf
schmidt-33 <33700475+schmidt-33@users.noreply.github.com>
2022-08-24 12:48:55 -0400

    made seperate css files for major components

--- src/components/Standings.tsx
+++ src/components/Standings.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route, Link, NavLink, Navigate } from 'react-router-dom';
 import Teams from './Teams';
 import Drivers from './Drivers';
-
+import '../css/standings.css'
 function Standings() {
   return (
     <div>
