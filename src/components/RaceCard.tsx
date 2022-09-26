914709709b1037cc16d564cdcb52ca6b674fe2d4
schmidt-33 <33700475+schmidt-33@users.noreply.github.com>
2022-09-26 13:12:13 -0400

    time now shows in respective time zone

--- src/components/RaceCard.tsx
+++ src/components/RaceCard.tsx
@@ -27,30 +27,46 @@ let countryCodes = new Map<string, string>([
 
 
 function RaceCard(round) {
+
+	const raceName = round.race.raceName
+
+	//convert times from UTC to respective time zone
+	const fp1Time = new Date(round.race.FirstPractice.date + " " + round.race.FirstPractice.time)
+	const fp2Time = new Date(round.race.SecondPractice.date + " " + round.race.SecondPractice.time)
+	const qualiTime = new Date(round.race.Qualifying.date + " " + round.race.Qualifying.time)
+	const raceTime = new Date(round.race.date + " " + round.race.time)
+	//fp3 and sprint times have to be done in there respectibe if statements as not every race has them
+
 	if (round.race.ThirdPractice) {
+
+		const fp3Time = new Date(round.race.ThirdPractice.date + " " + round.race.ThirdPractice.time)
+
 		return (
 			<div className='race-card' >
 				<span className={`fi fi-${countryCodes.get(round.race.Circuit.Location.country)}`} />
-				<div> {round.race.raceName} </div>
-				<div> FP1: {round.race.FirstPractice.time}</div>
-				<div> FP2: {round.race.SecondPractice.time}</div>
-				<div> FP3: {round.race.ThirdPractice.time}</div>
-				<div> Q: {round.race.Qualifying.time} </div>
-				<div> Race: {round.race.time} </div>
+				<div> {raceName} </div>
+				<div> <span className='session-name'>FP1: </span> {fp1Time.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})}</div>
+				<div> <span className='session-name'>FP2: </span> {fp2Time.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})}</div>
+				<div> <span className='session-name'>FP3: </span> {fp3Time.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})}</div>
+				<div> <span className='session-name'>Q: </span> {qualiTime.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})} </div>
+				<div> <span className='session-name'>Race: </span> {raceTime.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})} </div>
 			</div>
 		)
 	}
 
 	if (round.race.Sprint) {
+
+		const sprintTime = new Date(round.race.Sprint.date + " " + round.race.Sprint.time)
+
 		return (
 			<div className='race-card' >
 				<span className={`fi fi-${countryCodes.get(round.race.Circuit.Location.country)}`} />
-				<div> {round.race.raceName} </div>
-				<div> FP1: {round.race.FirstPractice.time}</div>
-				<div> FP2: {round.race.SecondPractice.time}</div>
-				<div> Sprint: {round.race.Sprint.time}</div>
-				<div> Q: {round.race.Qualifying.time} </div>
-				<div> Race: {round.race.time} </div>
+				<div> {raceName} </div>
+				<div> <span className='session-name'>FP1: </span> {fp1Time.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})}</div>
+				<div> <span className='session-name'>FP2: </span> {fp2Time.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})}</div>
+				<div> <span className='session-name'>Sprint: </span> {sprintTime.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})}</div>
+				<div> <span className='session-name'>Q: </span> {qualiTime.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})} </div>
+				<div> <span className='session-name'>Race: </span> {raceTime.toLocaleTimeString([], {hour:'numeric', minute:'numeric'})} </div>
 			</div>
 		)
 	}
