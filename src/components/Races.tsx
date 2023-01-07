2676dab73e65561702d839eeece4624d485a65c2
schmidt-33 <33700475+schmidt-33@users.noreply.github.com>
2023-01-06 21:11:54 -0500

    fixed standings loading 2022 by default

--- src/components/Races.tsx
+++ src/components/Races.tsx
@@ -3,23 +3,42 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import '../css/raceCard.css'
 import RaceCard from './RaceCard'
+import { currentYear } from '../Utils/SeasonYear';
 
 function Races() {
+	const [year, setYear] = useState(currentYear.toString())
 	const [Rounds, setRounds] = useState([])
 
 	useEffect(() => {
-axios.get('https://ergast.com/api/f1/current.json')
+		axios.get(`https://ergast.com/api/f1/${year}.json`)
 		.then(res => {
 			setRounds(res.data.MRData.RaceTable.Races)
 		})
 		.catch(err => {
 			console.log(err)
 		})
-	}, [])
+	}, [year])
+
+	//setup and populate an array for all seasons
+	let seasons:Array<Number> = []; 
+	for(let i = currentYear; i >= 1958; i--){
+	  seasons.push(i)
+	}
 	
+	const changeYearHandler = (event) => {
+		setYear(event.target.value)
+	  }
 
 	return (
 		<div className='race-container'>
+			<select onChange={changeYearHandler}>
+            {seasons.map(season => (
+              <option key={season.toString()} value={season.toString()}>
+                {season.toString()}
+              </option>
+            ))}
+
+      </select>
 			{
 				Rounds.map(round =>
 					<Link to={round.round} key={round.round} state={round}>
